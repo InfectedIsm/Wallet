61fca7231460ea073a3beb6bf7c590961d6e8f98
Salah-Eddine Ismail <52218361+InfectedIsm@users.noreply.github.com>
2022-07-06 23:36:02 +0200

    end of wallet training

--- client/src/utils.js
+++ client/src/utils.js
@@ -3,8 +3,28 @@ import Wallet from './contracts/Wallet.json'
 
 //instantiate a Web3 object
 const getWeb3 = () => {
-    return new Web3('http://localhost:9545');
-};
+    //in lesson 14 we change the line below
+    //return new Web3('http://localhost:9545');
+    return new Promise( (resolve, reject) => {
+        window.addEventListener('load', async () => {          
+        if (window.ethereum) {
+            const web3 = new Web3(window.ethereum);
+            try {
+                await window.ethereum.enable();
+                resolve(web3);
+            } catch (error) {
+                reject(error);
+            }
+        } else if(window.web3) {
+            const web3 = window.web3;
+            console.log("Injected web3 detected.");
+            resolve(web3);
+        } else {
+            reject('Must Install Metamask');
+        }
+        });
+    });
+    };
 
 //contract instance
 //object that is produced by web3 and allow you to interact with a smart contract
