3865ea5e4da72d1747a5dac470bca22c3538750c
InfectedIsm <52218361+InfectedIsm@users.noreply.github.com>
2022-12-08 21:35:28 +0100

    added tests + removed ethereum-waffle package

--- test/wallet.js
+++ test/wallet.js
@@ -8,6 +8,7 @@ const { expect } = require("chai");
 // Using this simplifies your tests and makes them run faster, by taking
 // advantage or Hardhat Network's snapshot functionality.
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { ethers } = require("hardhat");
 
 // `describe` is a Mocha function that allows you to organize your tests.
 // Having your tests organized makes debugging them easier. All Mocha
@@ -22,79 +23,98 @@ describe("Wallet contract", function () {
 
     const walletFactory = await ethers.getContractFactory("Wallet");
     const [signer0, signer1, signer2, signer3, signer4] = await ethers.getSigners();
-    addr0 = signer0.address;
-    addr1 = signer1.address;
-    addr2 = signer2.address;
-    addr3 = signer3.address;
-    addr4 = signer4.address;
-    console.log("0 :" +addr0);
-    console.log("1 :" +addr1);
-    console.log("2 :" +addr2);
-    console.log("3 :" +addr3);
+    // const signers = await ethers.getSigners(); // works too with signers[n] 
 
-    const wallet = await walletFactory.deploy([addr0, addr1, addr2], 2);
+    const wallet = await walletFactory.deploy([signer0.address, signer1.address, signer2.address], 2);
 
     await wallet.deployed();
 
+    //sending 10 ETH to contract
+    await signer0.sendTransaction({
+            from: signer0.address,
+            to: wallet.address,
+            value: ethers.utils.parseEther("10.0")
+          });
+
+    console.log("contract starting balance : %f ETH", ethers.utils.formatEther(await wallet.provider.getBalance(wallet.address)));
+
     // Fixtures can return anything you consider useful for your tests
-    return { 
-      walletFactory, wallet,
-      addr0, addr1, addr2, addr3, addr4,
-      signer0, signer1, signer2, signer3, signer4
+    return { walletFactory, wallet,
+       signer0, signer1, signer2, signer3, signer4
        };
   }
 
   // You can nest describe calls to create subsections.
-  describe("Deployment", function () {
+  describe("Transfer verification", function () {
     // `it` is another Mocha function. This is the one you use to define your
     // tests. It receives the test name, and a callback function.
 
     it('should have correct approvers and quorum', async () => {
 
-      const { wallet, addr0, addr1, addr2 } = await loadFixture(deployTokenFixture);
+      const { wallet, signer0, signer1, signer2 } = await loadFixture(deployTokenFixture);
 
       const approvers = await wallet.getApprovers();
 
       const quorum = await wallet.quorum();
 
       expect(approvers.length,'number of approvers incorrect').to.equal(3) // === 1, "number of approvers incorrect");
-      expect(approvers[0], "(at least) one of the approvers is not correct").to.equal(addr0);
-      expect(approvers[1], "(at least) one of the approvers is not correct").to.equal(addr1);
-      expect(approvers[2], "(at least) one of the approvers is not correct").to.equal(addr2);
+      expect(approvers[0], "(at least) one of the approvers is not correct").to.equal(signer0.address);
+      expect(approvers[1], "(at least) one of the approvers is not correct").to.equal(signer1.address);
+      expect(approvers[2], "(at least) one of the approvers is not correct").to.equal(signer2.address);
       expect(quorum, "quorum value not correct").to.equal(2);
     });
 
     it('should create transfers', async () => {
-      const { wallet, addr0, addr3 } = await loadFixture(deployTokenFixture);
+      const { wallet, signer0, signer3 } = await loadFixture(deployTokenFixture);
 
-      await wallet.createTransfer(100, addr3, {from: addr0});
+      await wallet.createTransfer(100, signer3.address, {from: signer0.address});
       const transfers = await wallet.getTransfers();
       //here numbers are compared with strings because they are in a struct in solidity
       //stupid but that's how it is
       expect(transfers.length, "number of transfer incorrect").to.equal(1);
       expect(transfers[0].id, "wrong transfer id").to.equal(0);
       expect(transfers[0].amount, "wrong amount").to.equal(100);
-      expect(transfers[0].to, "wrong recipient").to.equal(addr3);
+      expect(transfers[0].to, "wrong recipient").to.equal(signer3.address);
       expect(transfers[0].approvals, "wrong num of approvals").to.equal(0);
       expect(transfers[0].sent, "wrong sent status").to.be.false;
     });
 
     it('should NOT create transfers if sender is not approved', async() => {
-      const { wallet, addr4, signer3 } = await loadFixture(deployTokenFixture);
-      await expect(wallet.connect(signer3).createTransfer(100, addr4)
+      const { wallet, signer3, signer4 } = await loadFixture(deployTokenFixture);
+      await expect(wallet.connect(signer3).createTransfer(100, signer4.address)
         ).to.be.reverted;
     });
 
     it('should increment approval', async () => {
-      const { wallet, addr0, addr1, signer1 } = await loadFixture(deployTokenFixture);
-      await wallet.createTransfer(100, addr1, {from: addr0});
-      let transfer = await wallet.getTransfers();
-      expect(transfer[0].approvals, "expecting 0 approvals before approved").to.equal(0);
-      await wallet.connect(signer1).approveTransfer(0, {from: addr1});
-      transfer = await wallet.getTransfers();
-      expect(transfer[0].approvals, "expecting 1 approvals after approved").to.equal(1);
+      const { wallet, signer0, signer1} = await loadFixture(deployTokenFixture);
+      
+      await wallet.connect(signer0).createTransfer(100, signer1.address, {from: signer0.address});
+      let transfers = await wallet.getTransfers();
+      expect(transfers[0].approvals, "expecting 0 approvals before approved").to.equal(0);
+      
+      await wallet.connect(signer1).approveTransfer(transfers[0].id, {from: signer1.address});
+      transfers = await wallet.getTransfers();
+      expect(transfers[0].approvals, "expecting 1 approval after approved").to.equal(1);
+      expect(transfers[0].sent, "sent flag should be false").to.equal(false);
+      const walletBalance = ethers.utils.formatEther(await wallet.provider.getBalance(wallet.address));
+      expect(walletBalance, "wallet balance is wrong").to.equal("10.0")
     });
 
+    it('should send transfer if quorum reached', async () => {
+      const {wallet, signer0, signer1, signer2} = await loadFixture(deployTokenFixture);
+
+      await wallet.createTransfer(ethers.utils.parseEther("1"), signer2.address, {from: signer0.address});
+      let transfers = await wallet.getTransfers();
+      await wallet.connect(signer0).approveTransfer(transfers[0].id, {from: signer0.address});
+      await wallet.connect(signer1).approveTransfer(transfers[0].id, {from: signer1.address});
+      transfers = await wallet.getTransfers();
+
+      expect(transfers[0].approvals, "expecting 2 approvals").to.equal(2);
+      expect(transfers[0].sent, "expecting sent flag true").to.equal(true);
+      const walletBalance = ethers.utils.formatEther(await wallet.provider.getBalance(wallet.address));
+      expect(walletBalance, "wallet balance is wrong").to.equal("9.0")
+
+    });
   });
 
   // describe("Transactions", function () {
--- test/zwallet_old.js
+++ test/zwallet_old.js
@@ -16,65 +16,66 @@ contract ('Wallet', (accounts) => {
 
     //these are our tests
 
-    it('should have correct approvers and quorum', async () => {
-        const approvers = await wallet.getApprovers();
-        const quorum = await wallet.quorum();
-        assert(approvers.length === 3, "number of approvers incorrect");
-        assert(approvers[0] == accounts[0], "(at least) one of the approvers is not correct");
-        assert(approvers[1] == accounts[1], "(at least) one of the approvers is not correct");
-        assert(approvers[2] == accounts[2], "(at least) one of the approvers is not correct");
-        //toNumber js method must be used because JS don't understand Solidity numbers
-        assert(quorum.toNumber() === 2, "quorum value not correct");
-    });
+    // it('should have correct approvers and quorum', async () => {
+    //     const approvers = await wallet.getApprovers();
+    //     const quorum = await wallet.quorum();
+    //     assert(approvers.length === 3, "number of approvers incorrect");
+    //     assert(approvers[0] == accounts[0], "(at least) one of the approvers is not correct");
+    //     assert(approvers[1] == accounts[1], "(at least) one of the approvers is not correct");
+    //     assert(approvers[2] == accounts[2], "(at least) one of the approvers is not correct");
+    //     //toNumber js method must be used because JS don't understand Solidity numbers
+    //     assert(quorum.toNumber() === 2, "quorum value not correct");
+    // });
 
-    it('should create transfers', async () => {
-        await wallet.createTransfer(100, accounts[5], {from: accounts[0]});
-        const transfers = await wallet.getTransfers();
-        //here numbers are compared with strings because they are in a struct in solidity
-        //stupid but that's how it is
-        assert(transfers.length === 1, "number of transfer incorrect");
-        assert(transfers[0].id === '0', "wrong transfer id");
-        assert(transfers[0].amount === '100', "wrong amount");
-        assert(transfers[0].to === accounts[5], "wrong recipient");
-        assert(transfers[0].approvals === '0', "wrong num of approvals");
-        assert(transfers[0].sent === false, "wrong sent status");
-    });
+    // it('should create transfers', async () => {
+    //     await wallet.createTransfer(100, accounts[5], {from: accounts[0]});
+    //     const transfers = await wallet.getTransfers();
+    //     //here numbers are compared with strings because they are in a struct in solidity
+    //     //stupid but that's how it is
+    //     assert(transfers.length === 1, "number of transfer incorrect");
+    //     assert(transfers[0].id === '0', "wrong transfer id");
+    //     assert(transfers[0].amount === '100', "wrong amount");
+    //     assert(transfers[0].to === accounts[5], "wrong recipient");
+    //     assert(transfers[0].approvals === '0', "wrong num of approvals");
+    //     assert(transfers[0].sent === false, "wrong sent status");
+    // });
 
     //here we're using the test-helpers library helper function : expectRevert
     //we're telling truffle that the test passes if the call is reverted with the error we specify
     //as the second parameter
-    it('should NOT create transfers if sender is not approved', async() => {
-        await expectRevert(
-             wallet.createTransfer(100, accounts[5], {from: accounts[4]}),
-            'only approver allowed'
-            );
-    });
+    
+    // it('should NOT create transfers if sender is not approved', async() => {
+    //     await expectRevert(
+    //          wallet.createTransfer(100, accounts[5], {from: accounts[4]}),
+    //         'only approver allowed'
+    //         );
+    // });
 
-    it('should increment approval', async () => {
-        await wallet.createTransfer(100, accounts[5], {from: accounts[0]});
-        await wallet.approveTransfer(0);
-        const transfers = await wallet.getTransfers();
-        const balance = await web3.eth.getBalance(wallet.address);
-        assert(transfers[0].approvals === '1', "wrong approval value");
-        assert(transfers[0].sent === false, "sent should be false");
-        assert(balance === '1000');
-    });
+    // it('should increment approval', async () => {
+    //     await wallet.createTransfer(100, accounts[5], {from: accounts[0]});
+    //     await wallet.approveTransfer(0);
+    //     const transfers = await wallet.getTransfers();
+    //     const balance = await web3.eth.getBalance(wallet.address);
+    //     assert(transfers[0].approvals === '1', "wrong approval value");
+    //     assert(transfers[0].sent === false, "sent should be false");
+    //     assert(balance === '1000');
+    // });
 
-    it('should send transfer if quorum reached', async () => {
-        const balanceBefore = await web3.utils.toBN(await web3.eth.getBalance(accounts[6]));
-        await wallet.createTransfer(100, accounts[6], {from: accounts[0]});
-        // 2 approvals to meet the quorum
-        await wallet.approveTransfer(0,{from : accounts[0]});
-        await wallet.approveTransfer(0,{from : accounts[1]});
-        const balanceAfter = await web3.utils.toBN(await web3.eth.getBalance(accounts[6]));
-        assert(balanceAfter.sub(balanceBefore).toNumber() === 100, "wrong recipient balance");
+    // it('should send transfer if quorum reached', async () => {
+    //     const balanceBefore = await web3.utils.toBN(await web3.eth.getBalance(accounts[6]));
+    //     await wallet.createTransfer(100, accounts[6], {from: accounts[0]});
+    //     // 2 approvals to meet the quorum
+    //     await wallet.approveTransfer(0,{from : accounts[0]});
+    //     await wallet.approveTransfer(0,{from : accounts[1]});
+    //     const balanceAfter = await web3.utils.toBN(await web3.eth.getBalance(accounts[6]));
+    //     assert(balanceAfter.sub(balanceBefore).toNumber() === 100, "wrong recipient balance");
         
-        const transfers = await wallet.getTransfers();
-        const walletBalance = await web3.eth.getBalance(wallet.address);
-        assert(transfers[0].approvals === '2', "wrong approval value");
-        assert(transfers[0].sent === true, "sent should be false");
-        assert(walletBalance === '900',"wallet got wrong balance");
-    });
+    //     const transfers = await wallet.getTransfers();
+    //     const walletBalance = await web3.eth.getBalance(wallet.address);
+    //     assert(transfers[0].approvals === '2', "wrong approval value");
+    //     assert(transfers[0].sent === true, "sent should be false");
+    //     assert(walletBalance === '900',"wallet got wrong balance");
+    // });
 
     it('should NOT approve transfer if sender is not approved', async () => {
         await wallet.createTransfer(100, accounts[5], {from: accounts[0]});
